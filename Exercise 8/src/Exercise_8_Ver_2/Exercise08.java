ce9763809353d8d84ff2dfe0728db858c5fd6588
Hygor Costa <cost0142@algonquinlive.com>
2021-08-03 22:49:47 -0300

    Push / Commit ( First )

---
+++ Exercise 8/src/Exercise_8_Ver_2/Exercise08.java
@@ -0,0 +1,27 @@
+package Exercise_8_Ver_2;
+/*
+
+ * Student Name: Hygor Costa
+ * Lab Professor: Stanley Pieda (Professor)
+ * Due Date: The due date: 09/Aug/2021
+ * Description: Sample Solution Exercise 08 (21S) Version 1
+ */
+
+/*
+ * Simple driver program, it instantiates an EggCounter, then
+ * calls methods enterEggs() and printReport() in sequence.
+ */
+public class Exercise08 {
+
+	/*
+	 * Entry point for the application, command line arguments are not used for this
+	 * program.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Exercise 08 (21S) Version 2");
+		EggCounter counter = new EggCounter();
+		counter.enterEggs();
+		counter.printReport();
+		System.out.println("Program by Hygor costa");
+	}
+}
---
+++ Exercise 8/src/Exercise_8_Ver_3/Exercise08.java
@@ -0,0 +1,27 @@
+package Exercise_8_Ver_3;
+/*
+
+ * Student Name: Stanley Pieda (Professor)
+ * Lab Professor: Stanley Pieda (Professor)
+ * Due Date: The due date
+ * Description: Sample Solution Exercise 08 (21S) Version 3
+ */
+
+/*
+ * Simple driver program, it instantiates an EggCounter, then
+ * calls methods enterEggs() and printReport() in sequence.
+ */
+public class Exercise08 {
+	
+	/*
+	 * Entry point for the application, command line arguments are not
+	 * used for this program.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Exercise 08 (21S) Version 3");
+		EggCounter counter = new EggCounter();
+		counter.enterEggs();
+		counter.printReport();
+		System.out.println("Program by Hygor Costa");
+	}
+}
